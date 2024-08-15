995f7627667595b69fa522d459616ab79aed93e7
JOSTING JONNY MOLINA HERNÁNDEZ <153528729+JostingHer@users.noreply.github.com>
2024-08-15 18:01:55 +0200

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Fira_Code } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import ProviderTheme from "./utils/ProviderTheme";
 
 const inter = Inter({ subsets: ["latin"] });
+const firaCode = Fira_Code({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${firaCode.className} h-full bg-amber-50 text-indigo-950 dark:bg-slate-950 dark:text-amber-50 dark:selection:bg-purple-500`}
+    >
+          <ProviderTheme>
+                <Navbar></Navbar>
+                <main className='mx-auto max-w-5xl px-6'>
+                    {children}
+                </main>  
+          </ProviderTheme> 
+      </body>
     </html>
   );
 }
