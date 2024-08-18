fafa2a4c2a0523e51d17b6595733f5f371d0595b
JOSTING JONNY MOLINA HERNÁNDEZ <153528729+JostingHer@users.noreply.github.com>
2024-08-18 17:00:27 +0200

    updated slug

--- app/(client)/posts/[slug]/page.tsx
+++ app/(client)/posts/[slug]/page.tsx
@@ -42,7 +42,7 @@ const page = async ({params}: PropsArticlePage) => {
             </span>
             <div className="mt-5">
                 {post?.tags?.map((tag) => (
-                <Link key={tag?._id} href={`/tag/${tag.slug.current}`}>
+                <Link key={tag?._id} href={`/tags/${tag.slug.current}`}>
                     <span className="mr-2 p-1 rounded-sm text-sm lowercase dark:bg-gray-950 border dark:border-gray-900">
                     #{tag.name}
                     </span>
@@ -84,6 +84,7 @@ prose-li:ml-4
 `;
 
 
+export const revalidate = 60;
 
 async function getPostBySlug(slug: string): Promise<Post> {
     
